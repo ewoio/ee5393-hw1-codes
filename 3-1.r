d95bdd5c61386b27c41316f6060199f01200ddd2
Yiyuan Qin <qin00083@umn.edu>
2026-02-25 15:54:46 -0600

    upload

---
+++ 3-1.r
@@ -0,0 +1,11 @@
+bL 1 Yin 2: aL 1 bL 1 Yin 2 : 0.01
+aL 1 Yin 2: cL 1 YinP 1 aL 1 : 100000
+cL 2 : cL 1 : 100000
+aL 1 : WL 1 : 10000
+YinP 1 : Yin 1 : 1000
+cL 1 : m 1 : 100
+m 1 : Y 1 : 100000
+X 1: a 1: 0.0001
+a 1 Y 1: a 1 Yp 1 Z 1 : 100000
+a 1 : W 1 : 10000
+Yp 1 : Y 1 : 1000
---
+++ 3-2.r
@@ -0,0 +1,11 @@
+bL 1 Xin 2: aL 1 bL 1 Xin 2 : 0.01
+aL 1 Xin 2: cL 1 XinP 1 aL 1 : 100000
+cL 2 : cL 1 : 100000
+aL 1 : WL 1 : 10000
+XinP 1 : Xin 1 : 1000
+cL 1 : mL 1 : 100
+mL 1 : L 1 : 100000
+L 1: a 1: 0.01
+a 1 Y 1: a 1 Yp 2 : 1000000
+a 1 : W 1 : 100000
+Yp 1 : Y 1 : 1000
